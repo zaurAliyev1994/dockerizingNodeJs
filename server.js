479427874160f218ff555323e8b17934814b74a9
Zaur Aliyev <z.aliyev@iteratia.com>
2018-02-22 17:56:23 +0400

    docker config

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Hello from docker\n');
 });
 
 app.listen(PORT, HOST);
