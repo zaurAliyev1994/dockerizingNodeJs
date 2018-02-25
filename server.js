5dd0d8e2c784fff942a9d00a0e0bf601560ebe96
Zaur Aliyev <z.aliyev@iteratia.com>
2018-02-26 00:34:52 +0400

    configs

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello from aws docker\n');
+  res.send('Hello from aws docker88\n');
 });
 
 app.listen(PORT, HOST);
