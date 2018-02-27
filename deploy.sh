37835de044c111f95ef3097782b752057300f2ed
Zaur Aliyev <z.aliyev@iteratia.com>
2018-02-27 07:16:31 +0400

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,77 +1,45 @@
 #!/usr/bin/env bash
+sudo apt-get install jq
+CLUSTER='trashy-cluster'
+CONTAINER_INSTANCE='fda9944f-87b4-4788-a574-9003c2471ee1'
+DOCKER_IMAGE='docker-repo'
+TASK='trashy-task-2'
+SERVICE='trashy-service'
+TAG='latest'
+FAMILY='trashy-task-2'
+AWS_ACCESS_KEY_ID='AKIAJTXQBWDC6VSNY6AQ'
+AWS_SECRET_ACCESS_KEY='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'
+AWS_REGION='eu-central-1'
+AWS_ACCOUNT_ID='132868288379'
+aws configure set region $AWS_REGION
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+$(aws ecr get-login --no-include-email --region $AWS_REGION)
+docker build -t app .
+docker tag app $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$DOCKER_IMAGE
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$DOCKER_IMAGE
 
-# more bash-friendly output for jq
-JQ="jq --raw-output --exit-status"
+task_template='[
+  {
+    "name": "%s",
+    "image": "%s.dkr.ecr.eu-central-1.amazonaws.com/%s:%s",
+    "essential": true,
+    "memory": 128,
+    "portMappings": [
+      {
+        "containerPort": 8080,
+        "hostPort": 80
+      }
+    ]
+  }
+]'
+echo $task_template
+task_def=$(printf "$task_template" $TASK $AWS_ACCOUNT_ID $DOCKER_IMAGE $TAG)
+echo "START REGISTER TASK DEFFFFFFFFFFFFFFF"
+aws ecs register-task-definition --container-definitions "$task_def" --family $FAMILY
+echo "START UPDATE SERVICEEEEEEEEEEEEEEEE"
+aws ecs update-service --cluster $CLUSTER --service $SERVICE --task-definition trashy-task-2
 
-configure_aws_cli(){
-	aws configure set region $AWS_REGION
-    aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-    aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-}
-
-deploy_cluster() {
-    CLUSTER='trashy-cluster-2'
-    DOCKER_IMAGE='docker-repo'
-    TASK='trashy-task-2'
-    SERVICE='trashy-service-2'
-    TAG='latest'
-    FAMILY='trashy-task-2'
-    make_task_def
-    register_definition
-    if [[ $(aws ecs update-service --cluster $CLUSTER --service $SERVICE --task-definition $revision | \
-                   $JQ '.service.taskDefinition') != $revision ]]; then
-        echo "Error updating service."
-        return 1
-    fi
-
-    # wait for older revisions to disappear
-    # not really necessary, but nice for demos
-    for attempt in {1..30}; do
-        if stale=$(aws ecs describe-services --cluster $CLUSTER --services $SERVICE | \
-                       $JQ ".services[0].deployments | .[] | select(.taskDefinition != \"$revision\") | .taskDefinition"); then
-            echo "Waiting for stale deployments:"
-            echo "$stale"
-            sleep 5
-        else
-            echo "Deployed!"
-        fi
-    done
-    echo "Service update took too long."
-}
-
-make_task_def(){
-  task_template='[
-              {
-                "name": "%s",
-                "image": "%s.dkr.ecr.us-east-1.amazonaws.com/%s:%s",
-                "essential": true,
-                "memory": 128,
-                "portMappings": [
-                  {
-                    "containerPort": 8080,
-                    "hostPort": 80
-                  }
-                ]
-              }
-            ]'
-            task_def=$(printf "$task_template" $TASK $AWS_ACCOUNT_ID $DOCKER_IMAGE $CIRCLE_SHA1)
-}
-
-push_ecr_image(){
-    eval $(aws ecr get-login --no-include-email --region $AWS_REGION)
-    docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$DOCKER_IMAGE:$CIRCLE_SHA1
-}
-
-register_definition() {
-
-    if revision=$(aws ecs register-task-definition --container-definitions "$task_def" --family $FAMILY | $JQ '.taskDefinition.taskDefinitionArn'); then
-        echo "Revision: $revision"
-    else
-        echo "Failed to register task definition"
-        return 1
-    fi
-
-}
-configure_aws_cli
-push_ecr_image
-deploy_cluster
+RUNNING_TASK=$(aws ecs list-tasks --cluster trashy-cluster | jq -r '.taskArns | .[0]')
+aws ecs stop-task --task $RUNNING_TASK  --cluster trashy-cluster
+aws ecs start-task --task-definition trashy-task-2  --cluster trashy-cluster --container-instances $CONTAINER_INSTANCE
